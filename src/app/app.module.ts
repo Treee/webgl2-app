0360847d060761e32b9e4be9fbaa251d83dcad5a
Treee <jonathon.lindemann@gmail.com>
2018-03-02 12:58:56 -0700

    adding renderer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ShaderProgramService } from './services/shader-program/shader-program.service';
 import { TestComponent } from './test/test.component';
+import { RendererComponent } from './renderer/renderer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    RendererComponent
   ],
   imports: [
     BrowserModule
