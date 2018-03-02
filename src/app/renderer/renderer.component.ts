491a2a17be9562a99486c2ed779e079a90c0d0a6
Treee <jonathon.lindemann@gmail.com>
2018-03-02 13:29:27 -0700

    commented the renderer to ensure i remember what stuff does

--- src/app/renderer/renderer.component.ts
+++ src/app/renderer/renderer.component.ts
@@ -28,20 +28,30 @@ export class RendererComponent implements OnInit, AfterViewInit {
   }
 
   initCanvas() {
+    // get the canvas from the html
     const canvasEl: HTMLCanvasElement = this.canvasRef.nativeElement;
+
+    // get the webgl 2 context
     this.gl = canvasEl.getContext('webgl2') as any;
     if (!this.gl) {
       return -1;
     }
+
+    // set the width and height of the canvas
     canvasEl.width = this.width;
     canvasEl.height = this.height;
 
+    // create the default shader program for a 2d program
     this.basicShaderProgram = new ShaderProgram(this.shaderService);
     const basicShader = this.basicShaderProgram.getBasic2dProgram(this.gl);
 
+    // set up attribute and uniforms
     const positionAttributeLocation = this.gl.getAttribLocation(basicShader, 'a_position');
     const resolutionUniformLocation = this.gl.getUniformLocation(basicShader, 'u_resolution');
+
+    // make a buffer for position data
     const positionBuffer = this.gl.createBuffer();
+    // bind the buffer to the gl context as an array type
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
 
     // three 2d points
@@ -53,23 +63,29 @@ export class RendererComponent implements OnInit, AfterViewInit {
       80, 20,
       80, 30,
     ];
-    this.gl.enableVertexAttribArray(positionAttributeLocation);
 
+    // send data the the buffer as type of array. static draw means it doesnt change
     this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(positions), this.gl.STATIC_DRAW);
 
+    // make a vertex array (this is so we layer data in a single array)
     const vao = this.gl.createVertexArray();
+
+    // bind to the vertex array we will buffer data to
     this.gl.bindVertexArray(vao);
+
+    // enable an attribute that was created above (in this case, possition attrib)
     this.gl.enableVertexAttribArray(positionAttributeLocation);
 
-    const size = 2;          // 2 components per iteration
-    const type = this.gl.FLOAT;   // the data is 32bit floats
-    const normalize = false; // don't normalize the data
-    const stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-    let offset = 0;        // start at the beginning of the buffer
+    const size = 2;             // 2 components per iteration
+    const type = this.gl.FLOAT; // the data is 32bit floats
+    const normalize = false;    // don't normalize the data
+    const stride = 0;           // 0 = move forward size * sizeof(type) each iteration to get the next position
+    let offset = 0;             // start at the beginning of the buffer
+    // define how the gpu will interpret the array
     this.gl.vertexAttribPointer(
       positionAttributeLocation, size, type, normalize, stride, offset);
 
-
+    // set the viewport for the renderer
     this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
 
     // Clear the canvas
@@ -79,16 +95,11 @@ export class RendererComponent implements OnInit, AfterViewInit {
     // Tell it to use our program (pair of shaders)
     this.gl.useProgram(basicShader);
 
-    // Bind the attribute/buffer set we want.
-    this.gl.bindVertexArray(vao);
-
-    // Pass in the canvas resolution so we can convert from
-    // pixels to clipspace in the shader
+    // Pass in the canvas resolution so we can convert from pixels to clipspace in the shader
     this.gl.uniform2f(resolutionUniformLocation, this.gl.canvas.width, this.gl.canvas.height);
 
-    const primitiveType = this.gl.TRIANGLES;
     offset = 0;
     const count = 6;
-    this.gl.drawArrays(primitiveType, offset, count);
+    this.gl.drawArrays(this.gl.TRIANGLES, offset, count);
   }
 }
