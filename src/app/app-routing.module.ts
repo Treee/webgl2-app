926c5dec86d63ed24c599bb583c75774c0c24c86
Tree <tree.crypto@gmail.com>
2018-04-09 21:23:53 -0600

    added route to the 2d engine.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Router } from '@angular/router';
+
+import { Playground2dComponent } from './ui/playground-2d/playground-2d.component';
+
+const routes: Routes = [
+  { path: '2d', component: Playground2dComponent }
+];
 
 @NgModule({
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
