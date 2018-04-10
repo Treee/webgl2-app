7eacb79fa226a01438403b5325685ca651b97b78
Tree <tree.crypto@gmail.com>
2018-04-09 22:37:19 -0600

    ready for user input but need to enhance geometry2d to handle vector input for translating

--- src/app/ui/geo-wars-2d/geo-wars-2d.component.ts
+++ src/app/ui/geo-wars-2d/geo-wars-2d.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, HostListener } from '@angular/core';
 
 import { RendererComponent } from '../../renderer/renderer.component';
 import { Geometry2D } from '../../models/geometry2d';
+import { Vector3 } from 'three';
 
 @Component({
   selector: 'app-geo-wars-2d',
@@ -35,11 +36,40 @@ export class GeoWars2dComponent implements OnInit {
     this.player.setColor(Math.random(), Math.random(), Math.random(), 1);
     this.player.transformGeometry(this.renderer.projectionMatrix);
     this.renderableObjects.push(this.player);
-
   }
 
   redrawScreen() {
     this.renderer.drawFrame(0, this.renderer.shaderProgramInfo.basicShader, this.renderableObjects);
+  }
+
+  eventTriggered = false;
+  identity = new Vector3();
+  translationScale = 1;
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    // console.log('event', event.key);
+    this.eventTriggered = false;
+    if (event.key === 'w') {
+      // move forward      
+    }
+    else if (event.key === 'a') {
+      // strafe left
+    }
+    else if (event.key === 's') {
+      // move backwards
+    }
+    else if (event.key === 'd') {
+      // strafe right
+    }
+    else if (event.key === 'q') {
+      // rotate left
+    }
+    else if (event.key === 'e') {
+      // rotate right
+    }
+    if (this.eventTriggered) {
+      this.player.transformGeometry(this.renderer.projectionMatrix);
+    }
   }
 
 
