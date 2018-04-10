ef137900a835f750bb74d0d2cdc5fa86eb130eef
Tree <tree.crypto@gmail.com>
2018-04-09 21:43:53 -0600

    basic scaffolding and made the canvase larger

--- src/app/ui/geo-wars-2d/geo-wars-2d.component.ts
+++ src/app/ui/geo-wars-2d/geo-wars-2d.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { RendererComponent } from '../../renderer/renderer.component';
+import { Geometry2D } from '../../models/geometry2d';
 
 @Component({
   selector: 'app-geo-wars-2d',
@@ -6,6 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./geo-wars-2d.component.css']
 })
 export class GeoWars2dComponent implements OnInit {
+
+  @ViewChild('renderer') renderer: RendererComponent;
+
+  renderableObjects: Geometry2D[];
+  userInput: any = {};
+  width = 640;
+  height = 640;
 
   constructor() { }
 
