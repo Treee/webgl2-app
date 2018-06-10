1b0474e73dbc1bf5ce00151032549a15da3ea431
Tree <tree.crypto@gmail.com>
2018-06-10 13:04:14 -0600

    hooking up translate to ui

--- src/app/ui/playground-2d/playground-2d.component.ts
+++ src/app/ui/playground-2d/playground-2d.component.ts
@@ -45,14 +45,9 @@ export class Playground2dComponent implements AfterViewInit {
     const geometry = new BoxGeometry();
     geometry.createVertexArrayObject(this.renderer.gl, this.renderer.basicShader);
     geometry.setColor(new Vec4(1, 0, 0, 1));
-    // geometry.setScale(new Vec3(2, 2, 2));
+    geometry.setScale(new Vec3(2, 2, 2));
     geometry.translate(new Vec3(1, 0, 0));
     this.renderableObjects.push(geometry);
-    console.log('translation', geometry.getTranslationMatrix().prettyPrint());
-    console.log('scale', geometry.getScaleMatrix().prettyPrint());
-    console.log('rotate', geometry.getRotationMatrix().prettyPrint());
-    console.log('projection', this.renderer.projectionMatrix.prettyPrint());
-    console.log('transform', geometry.getTransform(this.renderer.projectionMatrix).prettyPrint());
 
     const geometry1 = new BoxGeometry();
     geometry1.createVertexArrayObject(this.renderer.gl, this.renderer.basicShader);
@@ -64,10 +59,6 @@ export class Playground2dComponent implements AfterViewInit {
 
   redrawScreen() {
     this.renderer.drawFrame(0, this.renderableObjects);
-    this.renderableObjects.forEach(obj => {
-      // console.log('objection postion', obj.getPosition());
-      // console.log('objection postion', obj.getTransform(this.renderer.projectionMatrix));
-    });
   }
 
   randomInt(range) {
@@ -85,26 +76,31 @@ export class Playground2dComponent implements AfterViewInit {
     };
     // this.ngAfterViewInit();
     clearInterval(this.gameLoop);
+    this.redrawScreen();
   }
 
   saveInput(type: string) {
+    let count = 1;
     this.renderableObjects.forEach(renderable => {
       if (type === 'translateX') {
-        renderable.translateByVector(new Vec3(this.userInput.x, 0, 0));
+        renderable.translate(new Vec3(this.userInput.x, renderable.getPosition().y, 1));
+        console.log('position', renderable.getTranslationMatrix().prettyPrint());
+        console.log('transform', renderable.getTransform(this.renderer.projectionMatrix).prettyPrint());
       } else if (type === 'translateY') {
-        renderable.translateByVector(new Vec3(0, this.userInput.y, 0));
+        renderable.translate(new Vec3(renderable.getPosition().x, this.userInput.y, 1));
+        console.log('position', renderable.getTranslationMatrix().prettyPrint());
       } else if (type === 'rotatecw') {
         renderable.rotate(this.userInput.rotatecw);
       } else if (type === 'rotateccw') {
         renderable.rotate(-this.userInput.rotateccw);
       } else if (type === 'scaleX') {
-        renderable.scaleByVector(new Vec3(this.userInput.scaleX, 0, 0));
+        renderable.setScale(new Vec3(this.userInput.scaleX * count, renderable.getScale().y, 0));
       } else if (type === 'scaleY') {
-        renderable.scaleByVector(new Vec3(0, this.userInput.scaleY, 0));
+        renderable.setScale(new Vec3(renderable.getScale().x, this.userInput.scaleY * count, 0));
       }
-      renderable.transformGeometry(this.renderer.projectionMatrix);
+      count++;
     });
-    this.renderer.drawFrame(0, this.renderableObjects);
+    this.redrawScreen();
   }
 
 }
