5f9886998db4a0ab62ad62f7d5cd533b92c3982a
Tree <tree.crypto@gmail.com>
2018-06-18 15:17:27 -0600

    some semblance of stability

--- src/app/ui/playground-2d/playground-2d.component.ts
+++ src/app/ui/playground-2d/playground-2d.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef, HostListener } from '@angular/core';
 import { BoxGeometry, Vec3, Vec4, RendererEngine } from 'tree-xyz-webgl2-engine';
 import { Vector4 } from 'three';
 
@@ -15,8 +15,8 @@ export class Playground2dComponent implements AfterViewInit {
   renderer: RendererEngine;
 
   userInput: any = {
-    x: 0,
-    y: 0,
+    x: 200,
+    y: 200,
     rotatecw: 0,
     rotateccw: 0,
     scaleX: 1,
@@ -34,7 +34,12 @@ export class Playground2dComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     this.renderer.initializeRenderer(this.canvasElement.nativeElement, this.width, this.height);
-    this.initializeDefaultRenderableObjects(5);
+    this.initializeDefaultRenderableObjects(1);
+    // this.startGameLoop();
+    this.redrawScreen();
+  }
+
+  startGameLoop() {
     this.gameLoop = setInterval(() => {
       this.redrawScreen();
     }, 60);
@@ -46,18 +51,28 @@ export class Playground2dComponent implements AfterViewInit {
     geometry.createVertexArrayObject(this.renderer.gl, this.renderer.basicShader);
     geometry.setColor(new Vec4(1, 0, 0, 1));
     geometry.setScale(new Vec3(2, 2, 2));
-    geometry.translate(new Vec3(1, 0, 0));
+    geometry.translate(new Vec3(200, 200, 0));
     this.renderableObjects.push(geometry);
 
-    const geometry1 = new BoxGeometry();
-    geometry1.createVertexArrayObject(this.renderer.gl, this.renderer.basicShader);
-    geometry1.setColor(new Vec4(0, 0, 1, 1));
-    geometry1.translate(new Vec3(10, 0, 0));
-    this.renderableObjects.push(geometry1);
+    // const geometry1 = new BoxGeometry();
+    // geometry1.createVertexArrayObject(this.renderer.gl, this.renderer.basicShader);
+    // geometry1.setColor(new Vec4(0, 0, 1, 1));
+    // geometry1.setScale(new Vec3(2, 2, 2));
+    // geometry1.translate(new Vec3(10, 0, 0));
+    // this.renderableObjects.push(geometry1);
 
   }
 
   redrawScreen() {
+    this.renderableObjects.forEach((renderable) => {
+      console.log('---Debug Info---');
+      console.log('trans', renderable.getTranslationMatrix().prettyPrint());
+      console.log('rotat', renderable.getRotationMatrix().prettyPrint());
+      console.log('scale', renderable.getScaleMatrix().prettyPrint());
+      console.log('transform', renderable.getTransform(this.renderer.projectionMatrix).prettyPrint());
+      console.log('transform', renderable.getTransform(this.renderer.projectionMatrix).toArray());
+      console.log();
+    });
     this.renderer.drawFrame(0, this.renderableObjects);
   }
 
@@ -79,26 +94,41 @@ export class Playground2dComponent implements AfterViewInit {
     this.redrawScreen();
   }
 
+  @HostListener('document:keyup', ['$event'])
+  @HostListener('document:keydown', ['$event'])
+  captureUserInput(keycode) {
+    let type = '';
+    if (keycode.code === 'KeyW') {
+      type = 'translateY';
+      this.userInput.y -= 1;
+    } else if (keycode.code === 'KeyS') {
+      type = 'translateY';
+      this.userInput.y += 1;
+    } else if (keycode.code === 'KeyA') {
+      type = 'translateX';
+      this.userInput.x -= 1;
+    } else if (keycode.code === 'KeyD') {
+      type = 'translateX';
+      this.userInput.x += 1;
+    }
+    this.saveInput(type);
+  }
+
   saveInput(type: string) {
-    let count = 1;
     this.renderableObjects.forEach(renderable => {
       if (type === 'translateX') {
         renderable.translate(new Vec3(this.userInput.x, renderable.getPosition().y, 1));
-        console.log('position', renderable.getTranslationMatrix().prettyPrint());
-        console.log('transform', renderable.getTransform(this.renderer.projectionMatrix).prettyPrint());
       } else if (type === 'translateY') {
         renderable.translate(new Vec3(renderable.getPosition().x, this.userInput.y, 1));
-        console.log('position', renderable.getTranslationMatrix().prettyPrint());
       } else if (type === 'rotatecw') {
         renderable.rotate(this.userInput.rotatecw);
       } else if (type === 'rotateccw') {
         renderable.rotate(-this.userInput.rotateccw);
       } else if (type === 'scaleX') {
-        renderable.setScale(new Vec3(this.userInput.scaleX * count, renderable.getScale().y, 0));
+        renderable.setScale(new Vec3(this.userInput.scaleX, renderable.getScale().y, 0));
       } else if (type === 'scaleY') {
-        renderable.setScale(new Vec3(renderable.getScale().x, this.userInput.scaleY * count, 0));
+        renderable.setScale(new Vec3(renderable.getScale().x, this.userInput.scaleY, 0));
       }
-      count++;
     });
     this.redrawScreen();
   }
