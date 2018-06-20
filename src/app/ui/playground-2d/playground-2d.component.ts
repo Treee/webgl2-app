16d602c72a20db24f9a0940b067401394c6bc694
Tree <tree.crypto@gmail.com>
2018-06-20 09:25:48 -0600

    working user input and starting to move things into one game loop for localization

--- src/app/ui/playground-2d/playground-2d.component.ts
+++ src/app/ui/playground-2d/playground-2d.component.ts
@@ -12,6 +12,8 @@ export class Playground2dComponent implements AfterViewInit {
   @ViewChild('glCanvas') canvasElement: ElementRef;
 
   renderableObjects: BoxGeometry[];
+  activeKeysMap: any;
+
   renderer: RendererEngine;
 
   userInput: any = {
@@ -28,19 +30,20 @@ export class Playground2dComponent implements AfterViewInit {
 
   constructor() {
     this.renderableObjects = [];
+    this.activeKeysMap = {};
     this.renderer = new RendererEngine();
   }
 
   ngAfterViewInit() {
     this.renderer.initializeRenderer(this.canvasElement.nativeElement, this.width, this.height);
     this.initializeDefaultRenderableObjects(1);
-    // this.startGameLoop();
+    this.startGameLoop();
     this.redrawScreen();
   }
 
   startGameLoop() {
     this.gameLoop = setInterval(() => {
-      this.redrawScreen();
+      this.oneGameLoop();
     }, 60);
   }
 
@@ -52,14 +55,10 @@ export class Playground2dComponent implements AfterViewInit {
     // geometry.setScale(new Vec3(2, 2, 2));
     geometry.translate(new Vec3(200, 200, 0));
     this.renderableObjects.push(geometry);
+  }
 
-    // const geometry1 = new BoxGeometry();
-    // geometry1.createVertexArrayObject(this.renderer.gl, this.renderer.basicShader);
-    // geometry1.setColor(new Vec4(0, 0, 1, 1));
-    // geometry1.setScale(new Vec3(2, 2, 2));
-    // geometry1.translate(new Vec3(10, 0, 0));
-    // this.renderableObjects.push(geometry1);
-
+  oneGameLoop() {
+    this.redrawScreen();
   }
 
   redrawScreen() {
@@ -81,37 +80,43 @@ export class Playground2dComponent implements AfterViewInit {
       scaleX: 1,
       scaleY: 1
     };
+    this.activeKeysMap = {};
     // this.ngAfterViewInit();
     clearInterval(this.gameLoop);
     this.redrawScreen();
   }
 
-  @HostListener('document:keyup', ['$event'])
   @HostListener('document:keydown', ['$event'])
-  captureUserInput(keycode) {
-    // console.log('key', keycode);
-    let type = '';
-    if (keycode.code === 'KeyW') {
-      type = 'translateY';
-      this.userInput.y -= 1;
-    } else if (keycode.code === 'KeyS') {
-      type = 'translateY';
-      this.userInput.y += 1;
-    } else if (keycode.code === 'KeyA') {
-      type = 'translateX';
-      this.userInput.x -= 1;
-    } else if (keycode.code === 'KeyD') {
-      type = 'translateX';
-      this.userInput.x += 1;
-    } else if (keycode.code === 'KeyQ') {
-      this.userInput.rotate -= 1;
-      type = 'rotateccw';
-    } else if (keycode.code === 'KeyE') {
-      this.userInput.rotate += 1;
-      type = 'rotatecw';
-    }
-    this.saveInput(type);
+  @HostListener('document:keyup', ['$event'])
+  userKeyPress(event) {
+    this.activeKeysMap[event.key] = (event.type === 'keydown');
+    // this.printKeyboardDebugInfo('test');
   }
+
+  // captureUserInput(keycode) {
+  //   // console.log('key', keycode);
+  //   let type = '';
+  //   if (keycode.code === 'KeyW') {
+  //     type = 'translateY';
+  //     this.userInput.y -= 1;
+  //   } else if (keycode.code === 'KeyS') {
+  //     type = 'translateY';
+  //     this.userInput.y += 1;
+  //   } else if (keycode.code === 'KeyA') {
+  //     type = 'translateX';
+  //     this.userInput.x -= 1;
+  //   } else if (keycode.code === 'KeyD') {
+  //     type = 'translateX';
+  //     this.userInput.x += 1;
+  //   } else if (keycode.code === 'KeyQ') {
+  //     this.userInput.rotate -= 1;
+  //     type = 'rotateccw';
+  //   } else if (keycode.code === 'KeyE') {
+  //     this.userInput.rotate += 1;
+  //     type = 'rotatecw';
+  //   }
+  //   this.saveInput(type);
+  // }
 
   saveInput(type: string) {
     this.renderableObjects.forEach(renderable => {
@@ -133,7 +138,7 @@ export class Playground2dComponent implements AfterViewInit {
     this.redrawScreen();
   }
 
-  printDebugInfo(renderable) {
+  printRenderableDebugInfo(renderable) {
     console.log('---Debug Info---');
     console.log('trans', renderable.getTranslationMatrix().prettyPrint());
     console.log('rotat', renderable.getRotationMatrix().prettyPrint());
@@ -141,6 +146,12 @@ export class Playground2dComponent implements AfterViewInit {
     console.log('transform', renderable.getTransform(this.renderer.projectionMatrix).prettyPrint());
     console.log('transform', renderable.getTransform(this.renderer.projectionMatrix).toArray());
     console.log();
+  }
+
+  printKeyboardDebugInfo(customMessage: string) {
+    console.log(customMessage);
+    console.log(`# of Keys currently pressed ${this.activeKeysMap}`, this.activeKeysMap);
+    console.log('End');
   }
 
 }
