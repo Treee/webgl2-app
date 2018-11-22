2a72d7ca7b30c365e749beb38be9f23ad2183a49
Treee <jonathon.lindemann@gmail.com>
2018-11-21 21:24:46 -0700

    grid represented correctly in game engine

--- src/app/ui/playground-2d/rts-map/rts-map.component.ts
+++ src/app/ui/playground-2d/rts-map/rts-map.component.ts
@@ -18,22 +18,9 @@ export class RtsMapComponent implements AfterViewInit {
   mapGrid: Grid2D;
   pathFinder: AStar;
 
-  playerRotation: number;
-
   renderableObjects: BoxGeometry[];
-  activeKeysMap: any;
 
   renderer: RendererEngine;
-
-  particles: Point2D[];
-
-  userInput: any = {
-    x: 200,
-    y: 200,
-    rotate: 0,
-    scaleX: 1,
-    scaleY: 1
-  };
 
   width = 400;
   height = 400;
@@ -42,14 +29,9 @@ export class RtsMapComponent implements AfterViewInit {
 
   constructor() {
     this.renderableObjects = [];
-    this.particles = [];
-    this.activeKeysMap = {};
     this.renderer = new RendererEngine();
-    this.playerRotation = 0;
     this.initializeGrid();
   }
-
-
 
   initializeGrid() {
     this.mapGrid = new Grid2D();
@@ -111,16 +93,6 @@ export class RtsMapComponent implements AfterViewInit {
     return color;
   }
 
-  initializeParticles(numParticles: number) {
-    for (let i = 0; i < numParticles; i++) {
-      const newPoint = new Point2D(100, 100);
-      newPoint.createVertexArrayObject(this.renderer.gl, this.renderer.basicShader);
-      this.particles.push(newPoint);
-      this.renderableObjects.push(newPoint);
-      // console.log('creating particle', newPoint);
-    }
-  }
-
   oneGameLoop() {
     // this.applyUserInput();
     this.redrawScreen();
@@ -131,50 +103,6 @@ export class RtsMapComponent implements AfterViewInit {
       // this.printRenderableDebugInfo(renderable);
     });
     this.renderer.drawFrame(0, this.renderableObjects);
-  }
-
-  randomInt(range) {
-    return Math.floor(Math.random() * range);
-  }
-
-  resetUserInput() {
-    this.userInput = {
-      x: 0,
-      y: 0,
-      rotate: 0,
-      scaleX: 1,
-      scaleY: 1
-    };
-    this.activeKeysMap = {};
-    // this.ngAfterViewInit();
-    clearInterval(this.gameLoop);
-    this.redrawScreen();
-  }
-
-  @HostListener('document:keydown', ['$event'])
-  @HostListener('document:keyup', ['$event'])
-  userKeyPress(event) {
-    this.activeKeysMap[event.key] = (event.type === 'keydown');
-  }
-
-  saveInput(type: string) {
-    this.renderableObjects.forEach(renderable => {
-      if (type === 'translateX') {
-        renderable.translate(new Vec3(this.userInput.x, renderable.getPosition().y, 1));
-      } else if (type === 'translateY') {
-        renderable.translate(new Vec3(renderable.getPosition().x, this.userInput.y, 1));
-      } else if (type === 'rotatecw') {
-        console.log('rotation', this.userInput.rotate);
-        renderable.rotate(this.userInput.rotate);
-      } else if (type === 'rotateccw') {
-        renderable.rotate(this.userInput.rotate);
-      } else if (type === 'scaleX') {
-        renderable.setScale(new Vec3(this.userInput.scaleX, renderable.getScale().y, 0));
-      } else if (type === 'scaleY') {
-        renderable.setScale(new Vec3(renderable.getScale().x, this.userInput.scaleY, 0));
-      }
-    });
-    this.redrawScreen();
   }
 
   printRenderableDebugInfo(renderable) {
@@ -189,9 +117,11 @@ export class RtsMapComponent implements AfterViewInit {
 
   printKeyboardDebugInfo(customMessage: string) {
     console.log(customMessage);
-    console.log(`# of Keys currently pressed ${this.activeKeysMap}`, this.activeKeysMap);
+    // console.log(`# of Keys currently pressed ${this.activeKeysMap}`, this.activeKeysMap);
     console.log('End');
   }
 
+  solveMaze() {
 
+  }
 }
